86f6dec0f35da19e8bf0af1112800cca64452f45
liu hongzhuang <691381755@qq.com>
2023-09-01 22:21:35 +0800

    🔧 build: add pinia and router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import { createPinia } from "pinia";
+import router from "./router";
+const pinia = createPinia();
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+
+app.mount("#app");
---
+++ src/router/index.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const Home = () => import("../views/Home.vue");
+
+const routes = [{ path: "/", component: Home }];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
