f1161dd34a011f416474f1cd525c2f129d301e2f
liuxz <691381755>
2023-09-12 22:47:53 +0800

    🔧 build: add eslint and prettier

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,6 @@
   <Footer></Footer>
 </template>
 <script setup lang="ts">
-import Header from "@/components/BaseHeader/index.vue";
-import Footer from "@/components/BaseFooter/index.vue";
+import Header from '@/components/BaseHeader/index.vue'
+import Footer from '@/components/BaseFooter/index.vue'
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import { createApp } from "vue";
-import "./style.css";
-import { createPinia } from "pinia";
-import router from "./router";
-const pinia = createPinia();
+import { createApp } from 'vue'
+import './style.css'
+import { createPinia } from 'pinia'
+import router from './router'
+const pinia = createPinia()
 
-import App from "./App.vue";
-const app = createApp(App);
+import App from './App.vue'
+const app = createApp(App)
 
-app.use(pinia);
-app.use(router);
+app.use(pinia)
 
-app.mount("#app");
+app.use(router)
+
+app.mount('#app')
