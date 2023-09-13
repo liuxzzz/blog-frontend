65c9f7f2ea84549b5dcfe0851776b50a7d2f3015
liu hongzhuang <691381755@qq.com>
2023-09-14 00:08:14 +0800

    ✨ feat(footer): add footer

--- src/components/BaseFooter/index.vue
+++ src/components/BaseFooter/index.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>Footer</div>
+  <div class="footer">
+    <span>This page designed and built with by Horizon Lin in 2022</span>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="less">
+.footer {
+  display: flex;
+  justify-content: center;
+  padding: 1.25rem;
+
+  font-size: 14px;
+  color: rgb(134, 142, 150);
+}
+</style>
